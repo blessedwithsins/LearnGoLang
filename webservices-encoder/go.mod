ef3527029295cf2a86874a75d20193a10471b476
blessedwithsins <uchihamadara1503@gmail.com>
2022-04-24 10:00:05 +0530

    Revert "Webserv encode"

--- webservices-encoder/go.mod
+++
@@ -1,3 +0,0 @@
-module webservices
-
-go 1.18
---
+++ webservices-urlquery/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+	"log"
+)
+
+func main () {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		names := r.URL.Query()["name"]
+		var name string
+		if len(names) == 1 {
+			name = names[0] 
+		}
+		w.Write([]byte("Hello " + name))
+	})
+
+	err := http.ListenAndServe(":4000", nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
