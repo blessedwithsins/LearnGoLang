9427320e50b9170088b3eaf62cc1733ed90606a6
blessedwithsins <piyushgaur123@gmail.com>
2022-06-28 09:05:10 +0530

    Adding lab files

---
+++ kodekloud-labs/go.mod
@@ -0,0 +1,3 @@
+module lab1-3.go
+
+go 1.18
---
+++ kodekloud-labs/lab1-1.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type Student interface {
+	getPercentage() int
+	getName()
+}
+
+type Undergrad struct {
+	name   string
+	grades []int
+}
+
+func (u Undergrad) getPercentage() int {
+	sum := 0
+	for _, v := range u.grades {
+		sum += v
+	}
+	return sum / len(u.grades)
+}
+
+func printPercentage(s Student) {
+	fmt.Println(s.getPercentage())
+}
+
+func main() {
+	grades := []int{90, 75, 80}
+	u := Undergrad{"Ross", grades}
+	printPercentage(u)
+}
